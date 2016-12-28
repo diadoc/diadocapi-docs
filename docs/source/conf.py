9fb1e937737e629aa67e3f353cd2daa4934ab39c
Andrey Tytskiy <andrew.tytsky@gmail.com>
2016-12-28 11:24:21 +0500

    fix: change index language

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -195,7 +195,7 @@ html_show_copyright = True
 # Sphinx supports the following languages:
 #   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
 #   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-html_search_language = 'ru'
+html_search_language = 'en'
 
 # A dictionary with options for the search language support, empty by default.
 # Now only 'ja' uses this config value
